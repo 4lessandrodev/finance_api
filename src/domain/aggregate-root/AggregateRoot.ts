7949448234bb217389271edeeb28b9da758e1fff
alessandro <alessandroadm@live.com>
2020-11-04 00:01:05 -0300

    refactor: remove try catch from aggregate root

--- src/domain/aggregate-root/AggregateRoot.ts
+++ src/domain/aggregate-root/AggregateRoot.ts
@@ -3,10 +3,8 @@ import { v4 as uuid } from 'uuid';
 
 export class AggregateRoot{
  validateId(id: string):string {
-  try {
+
    return (id === null || id === '' || id === undefined) ? uuid() : id;
-  } catch (error) {
-   return uuid();
-  }
+
  }
 }
