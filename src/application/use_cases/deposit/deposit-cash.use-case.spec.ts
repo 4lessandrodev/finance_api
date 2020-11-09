409f4993c61424da818d72e985a3a08bafae3e49
alessandro <alessandroadm@live.com>
2020-11-09 00:06:39 -0300

    test: add tests to deposit cash use case

--- src/application/use_cases/deposit/deposit-cash.use-case.spec.ts
+++ src/application/use_cases/deposit/deposit-cash.use-case.spec.ts
@@ -1,58 +1,18 @@
-import { IDepositCash } from '../../../domain/interfaces/entities/IDepositCash';
-import { DepositCashAccountDto } from '../../../dto/account/deposit-cash-account.dto';
-import { IAccount } from '../../../domain/interfaces/entities/IAccount';
-import { CreateAccountDto } from '../../../dto/account/create-account.dto';
-import { DeleteAccountDto } from '../../../dto/account/delete-account.dto';
-import { filterAccountDto } from '../../../dto/account/filter-account.dto';
-import { FindAccountDto } from '../../../dto/account/find-account.dto';
-import { UpdateAccountDto } from '../../../dto/account/update-account.dto';
 import { DepositCashUseCase } from './deposit-cash.use-case';
+import {fakeDeposit,fakeDepositRepo } from './../tests/fakeRepo';
+import { Deposit } from './../../../domain/entities/Deposit';
+
 
 
 describe('create account', () => {
 
- const fakeDeposit = ():IDepositCash => {
-  return {
-   agency: 1090,
-   account: 559020,
-   depositValue: 10
-  };
- };
+   it('should create a instance of account to make a deposit', () => {
+      const repository = fakeDepositRepo();
+      const depositDto = new Deposit(fakeDeposit());
+      const result = async ()=> await new DepositCashUseCase().execute(depositDto, repository);
+      expect(result).not.toThrow();
+   });
 
- const fakeAccount = ():IAccount => {
-  return {
-   id:'null',
-   agency: 1090,
-   account: 559020,
-   balance: 100,
-   name: 'Maraia'
-  };
- };
-
- const makeSut = () => {
-  const accounts:IAccount[] = [fakeAccount(), fakeAccount(), fakeAccount()];
-  return {
-   createAccount: async (account:CreateAccountDto):Promise<IAccount> => account,
-   updateAccount: async (account: UpdateAccountDto): Promise<IAccount> => account,
-   deleteAccount: async (id:DeleteAccountDto): Promise<void> => {id;},
-   findOneAccount: async (id: FindAccountDto): Promise<IAccount> => { console.log(id); return fakeAccount(); },
-   findManyAccount: async (search: filterAccountDto): Promise<IAccount[]> => { console.log(search); return accounts; },
-   depositCashOnAccount: async (deposit: DepositCashAccountDto, depositValue: number): Promise<IAccount> => {
-    return {
-     id:'idasd6546',
-     agency: deposit.agency,
-     account: 559020,
-     balance: 100 + depositValue,
-     name:'Hoao'
-    };}};
- };
-
-
- it('should create a instance of account to make a deposit', () => {
-  const repository = makeSut();
-    const depositDto = fakeDeposit();
-    const result = async ()=> await new DepositCashUseCase().execute(depositDto,repository);
-    expect(result).not.toThrow();
- });
-
+   // Ensure call credit domain event on make a deposit 
+   
 });
