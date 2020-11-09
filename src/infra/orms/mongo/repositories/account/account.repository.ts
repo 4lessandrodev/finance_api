9660d58aa5bab4afac39338119b626dbae00db10
alessandro <alessandroadm@live.com>
2020-11-08 23:51:03 -0300

    feat: create deposit repository

--- src/infra/orms/mongo/repositories/account/account.repository.ts
+++ src/infra/orms/mongo/repositories/account/account.repository.ts
@@ -1,20 +1,23 @@
-import { IAccount } from 'src/domain/interfaces/entities/IAccount';
-import { CreateAccountDto } from 'src/dto/account/create-account.dto';
-import { DeleteAccountDto } from 'src/dto/account/delete-account.dto';
-import { DepositCashAccountDto } from 'src/dto/account/deposit-cash-account.dto';
-import { filterAccountDto } from 'src/dto/account/filter-account.dto';
-import { FindAccountDto } from 'src/dto/account/find-account.dto';
-import { UpdateAccountDto } from 'src/dto/account/update-account.dto';
-import { IAccountRepo } from 'src/infra/interfaces/IAccountRepo';
+import { IAccount } from '../../../../../domain/interfaces/entities/IAccount';
+import { CreateAccountDto } from '../../../../../dto/account/create-account.dto';
+import { DeleteAccountDto } from '../../../../../dto/account/delete-account.dto';
+import { FilterAccountDto } from '../../../../../dto/account/filter-account.dto';
+import { FindAccountDto } from '../../../../../dto/account/find-account.dto';
+import { UpdateAccountDto } from '../../../../../dto/account/update-account.dto';
+
 import { EntityRepository, Repository } from 'typeorm';
 import { AccountSchema } from '../schemas/account.entity';
+import { IAccountRepository } from '../../../../../domain/interfaces/repositories/IAccountRepository';
+import { DepositCashOnMyAccountDto } from '../../../../../dto/account/deposit-cash-on-my-account.dto';
+import { MakeTransferenceDto } from '../../../../../dto/account/make-transference.dto';
 
 @EntityRepository(AccountSchema)
-export class AccountRepository extends Repository<AccountSchema> implements IAccountRepo{
+export class AccountRepository extends Repository<AccountSchema> implements IAccountRepository{
  createAccount: (account: CreateAccountDto) => Promise<IAccount>;
  updateAccount: (account: UpdateAccountDto) => Promise<IAccount>;
  deleteAccount: (id: DeleteAccountDto) => Promise<void>;
  findOneAccount: (id: FindAccountDto) => Promise<IAccount>;
- findManyAccount: (search: filterAccountDto) => Promise<IAccount[]>;
- depositCashOnAccount: (deposit: DepositCashAccountDto) => Promise<IAccount>;
+ findManyAccount: (search: FilterAccountDto) => Promise<IAccount[]>;
+ makeTransference: (transference: MakeTransferenceDto) => Promise<IAccount>;
+ depositCashOnMyAccount: (deposit: DepositCashOnMyAccountDto)=> Promise<IAccount>;
 }
---
+++ src/infra/orms/mongo/repositories/deposit/deposit.repository.ts
@@ -0,0 +1,13 @@
+import { IDepositCash } from '../../../../../domain/interfaces/entities/IDepositCash';
+import { IDepositCashRepository } from '../../../../../domain/interfaces/repositories/IDepositCashRepository';
+import { DepositCashAccountDto } from '../../../../../dto/deposit/deposit-cash-account.dto';
+import { EntityRepository, Repository } from 'typeorm';
+import { DepositSchema } from '../schemas/deposit.entity';
+
+@EntityRepository(DepositSchema)
+export class DepositRepository extends Repository<DepositSchema> implements IDepositCashRepository{
+ depositCashOnAccount(deposit: DepositCashAccountDto): Promise<IDepositCash> {
+  throw new Error('Method not implemented.' + deposit);
+ }
+ 
+}
