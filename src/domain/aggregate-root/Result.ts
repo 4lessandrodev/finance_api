ae022c560a679bb43635c6498f28853a68fa5ee9
alessandro <alessandroadm@live.com>
2020-11-09 22:47:27 -0300

    feat: add Result

---
+++ src/domain/aggregate-root/Result.ts
@@ -0,0 +1,86 @@
+export class Result<T>{
+ public isSuccess: boolean;
+ public isFailure: boolean;
+ public error: T | string;
+ private _value: T;
+ public constructor(isSuccess: boolean, error?: T | string, value?: T) {
+  if (isSuccess && error) {
+   throw new Error('InvalidOperation: Result cannot be successful and contain an error');
+  }
+  if (!isSuccess && !error) {
+   throw new Error('InvalidOperation: Fail, result  needs to contain an error message');
+  }
+  this.isSuccess = isSuccess;
+  this.isFailure = !isSuccess;
+  this.error = error;
+  this._value = value;
+  Object.freeze(this);
+ }
+ public getValue(): T{
+  if (!this.isSuccess) {
+   console.log(this.error);
+   throw new Error('Can not get the value of an error. Use errorValue to get it');
+  }
+  return this._value;
+ }
+ public errorValue (): T {
+  return this.error as T;
+ }
+ 
+ public static ok<U> (value?: U) : Result<U> {
+  return new Result<U>(true, null, value);
+ }
+ 
+ public static fail<U> (error: string): Result<U> {
+  return new Result<U>(false, error);
+ }
+ 
+ public static combine (results: Result<any>[]) : Result<any> {
+  for (const result of results) {
+   if (result.isFailure) return result;
+  }
+  return Result.ok();
+ }
+}
+
+export type Either<L, A> = Left<L, A> | Right<L, A>;
+
+export class Left<L, A> {
+ readonly value: L;
+ 
+ constructor(value: L) {
+  this.value = value;
+ }
+ 
+ isLeft(): this is Left<L, A> {
+  return true;
+ }
+ 
+ isRight(): this is Right<L, A> {
+  return false;
+ }
+}
+
+export class Right<L, A> {
+ readonly value: A;
+ 
+ constructor(value: A) {
+  this.value = value;
+ }
+ 
+ isLeft(): this is Left<L, A> {
+  return false;
+ }
+ 
+ isRight(): this is Right<L, A> {
+  return true;
+ }
+}
+
+export const left = <L, A>(l: L): Either<L, A> => {
+ return new Left(l);
+};
+
+export const right = <L, A>(a: A): Either<L, A> => {
+ return new Right<L, A>(a);
+};
