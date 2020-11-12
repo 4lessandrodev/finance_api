e3d1399ff8f9cd20b0388fde95d89d38f42534fe
alessandro <alessandroadm@live.com>
2020-11-11 23:51:42 -0300

    test: ensure create a valid monetary value object

---
+++ src/domain/value-objects/monetary.value-object.spec.ts
@@ -0,0 +1,15 @@
+import { MonetaryValueObject } from './monetary.value-object';
+
+describe('digit value object ', () => {
+ it('should fail if provide a negative value ', () => {
+  const idOrError = MonetaryValueObject.create(-1);
+  expect(idOrError.isFailure).toBe(true);
+  expect(idOrError.errorValue()).toBe('Invalid value. It should be positive');
+ });
+
+ it('should succesly create an id',  () => {
+  const idOrError =  MonetaryValueObject.create(100);
+  expect(idOrError.isFailure).toBe(false);
+  expect(idOrError.getValue().value).toBeGreaterThanOrEqual(100);
+ });
+});
